b5ed6e2a2f626f4505d08d5e5f950f21762b0ae2
Gilo17 <116527575+Gilo17@users.noreply.github.com>
2022-12-06 21:58:02 -0500

    Add files via upload

--- cypress/e2e/test/pageSort.cy.js
+++ cypress/e2e/test/pageSort.cy.js
@@ -1,62 +1,31 @@
-import Authentication from '../../page/login.page'
-import Product from '../../page/Product.page'
-import ProductData from '../../data/sort.data'
+import Authentication from '../page/login.page'
+import Product from '../page/Product.page'
+import ProductData from '../data/sort.data'
+import addToCart from '../page/addToCart.page'
+import Checkout from '../page/Checkout.page'
 
-describe('Sort', () => {
+
+describe('Sorting items names and prices', () => {
     beforeEach(() => {
         cy.visit('/')
-      })
+    })
 
-    it('should sort product list from A-Z', () => {
-        Authentication.login('standard_user','secret_sauce')
-        Product.selectSort(ProductData.sort['A to Z'])
-
-        // Sort data list based on name, from A to Z
+    it.only('should sort products from A-Z', () => {
+        Authentication.login('standard_user', 'secret_sauce')
         ProductData.products.sort()
-
-        cy.get(Product.itemsName).each(($elem, index) => {
+        cy.get(addToCart.cartItemName).each(($elem, index) => {
             expect($elem.text()).equal(ProductData.products[index].name)
         })
+
     })
-
-    it('should sort product list from Z-A', () => {
-        Authentication.login('standard_user','secret_sauce')
-        Product.selectSort(ProductData.sort['Z to A'])
-
-        // Sort data list based on name, from Z to A
-        ProductData.products.sort().reverse()
-
-        cy.get(Product.itemsName).each(($elem, index) => {
-            expect($elem.text()).equal(ProductData.products[index].name)
-        })
-    })
-
-    it('should sort product list from low to high', () => {
-        Auth.login('standard_user','secret_sauce')
-        Product.selectSort(ProductData.sort['Low to High'])
-
-        // Sort data list based on price, from low to high
+    it('should sort products from price low to high', () => {
+        Authentication.login('standard_user', 'secret_sauce')
+        Product.sortProducts(ProductsData.sort['Low to High'])
         ProductData.products.sort((a, b) => a.price - b.price)
-
-        cy.get(Product.itemsPrice).each(($elem, index) => {
+        cy.get(Checkout.itemPrice).each(($elem, index) => {
             expect($elem.text()).equal(`$${ProductData.products[index].price}`)
         })
+
     })
 
-    it('should sort product list from high to low', () => {
-        Authentication.login('standard_user','secret_sauce')
-        Product.selectSort(ProductData.sort['High to Low'])
-
-        // Sort data list based on price, from high to low
-        ProductData.products.sort((a, b) => b.price - a.price)
-
-        // Print the prices from the sorted list
-        // ProductData.products.forEach(($elem, index)=>{
-        //     cy.log($elem.price)
-        // })
-
-        cy.get(Product.itemsPrice).each(($elem, index) => {
-            expect($elem.text()).equal(`$${ProductData.products[index].price}`)
-        })
-    })
 })
