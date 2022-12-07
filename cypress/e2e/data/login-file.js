ca716f1ccb3aadd4ab14e91bf2e31afa497f0c7d
Gilo17 <116527575+Gilo17@users.noreply.github.com>
2022-12-06 22:00:28 -0500

    Add files via upload

---
+++ cypress/e2e/data/login-file.js
@@ -0,0 +1,21 @@
+
+
+module.exports = {
+    valid: {
+        userName: 'standard_user',
+        passWord: 'secret_sauce'
+    },
+    lockedOutUser: {
+        userName: 'locked_out_user',
+        passWord: 'secret_sauce',
+        errorMsg: 'Epic sadface: Sorry, this user has been locked out.'
+    },
+    problemUser: {
+        userName: 'problem_user',
+        passWord: 'secret_sauce'
+    },
+    performanceGlitchUser: {
+        userName: 'performance_glitch_user',
+        passWord: 'secret_sauce'
+    }
+}
